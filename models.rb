68494e2200cd1086ecad191944db3326e21da631
Dustin Leavins <dustinleavins@gmail.com>
2013-08-23 10:42:08 -0500

    Improved code documentation

--- models.rb
+++ models.rb
@@ -10,15 +10,21 @@ require 'bundler/setup'
 require 'sequel'
 require 'yaml'
 
+# Sequel models for Saltstorm
 module Models
+
+  # This if block ensures that this file can be required from both app.rb
+  # and the 'sequel' command-line utility.
   if (ENV['RACK_ENV'])
     Sequel.connect(YAML::load_file('config/database.yml')[ENV['RACK_ENV']])
   end
 
+  # A very simple regular expression for e-mail addresses.
   def self.email_regex
     /\A\S+@\S+\Z/
   end
 
+  # Represents a user
   class User < Sequel::Model
     plugin :validation_helpers
 
@@ -43,14 +49,17 @@ module Models
       validates_presence :password_salt
     end
 
+    # Plain-text representation of password
     def password
       return @password
     end
 
+    # Plain-text representation of password
     def password=(v)
       @password = v
     end
 
+    # Retrieves a Set of permissions
     def permissions
       if !self.permission_entry
         Set.new
@@ -59,19 +68,24 @@ module Models
       end
     end
 
+    # Sets a Set of permissions
     def permissions=(set)
       self.permission_entry = set.to_a.join(';')
     end
 
+    # Generates a digest given a password and hash
     def self.generate_password_digest(password, hash)
       return (Digest::SHA256.new << password << hash ).to_s
     end
 
+    # Generates a random salt value
     def self.generate_salt
       return SecureRandom.base64(8)
     end
   end
 
+  # Represents a bet made by a user. Bets are stored temporarily by
+  # the system and can only be for the current match.
   class Bet < Sequel::Model
     plugin :validation_helpers
 
@@ -88,6 +102,10 @@ module Models
       validates_presence :for_participant
     end
 
+    # User who made this bet.
+    # There can only be one user per bet. There can only be one bet per user.
+    # This is not a model association because I could not properly express this
+    # relationship as a Sequel association.
     def user
       return User.first(id: self.user_id)
     end
--- persistence.rb
+++ persistence.rb
@@ -6,7 +6,13 @@
 require 'json'
 require 'fileutils'
 
+# Module containing a static class for non-database persistence.
 module Persistence
+  
+  # Initializes non-database persistence.
+  # This initialization only needs to occur once as part of initial
+  # app setup, so users of the Persistence module probably do not
+  # need to call this method.
   def self.init_persistence()
     # Initialize tmp directory
     if File.exist?('tmp') && !File.directory?('tmp')
@@ -26,15 +32,20 @@ module Persistence
 
   end
 
+  # Persists current match status.
   class MatchStatusPersistence
     @@environment = ENV['RACK_ENV']
     @@match_data_file = "tmp/#{@@environment}/match_data.json"
     @@bet_file = "tmp/#{@@environment}/match_open"
 
+    # Name of file containing match data.
     def self.match_data_file
       return @@match_data_file
     end
 
+    # Retrieves match data from match_data_file.
+    # If match_data_file does not exist, this automatically
+    # initializes that file before returning its contents.
     def self.get_from_file
       match_data = nil
 
@@ -60,20 +71,24 @@ module Persistence
       return match_data
     end
 
+    # Saves match_data to the file.
     def self.save_file(new_match_data)
       File.open(@@match_data_file, 'w') do |f|
         f.write new_match_data.to_json
       end
     end
 
+    # Test to see if bids are currently open.
     def self.bids_open?
       return File.exist?(@@bet_file)
     end
 
+    # Open bids.
     def self.open_bids
       FileUtils.touch @@bet_file
     end
 
+    # Close bids.
     def self.close_bids
       FileUtils.rm @@bet_file, force: true
     end
