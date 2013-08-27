ca66cd855af27a159bf65f0b06be9d237113df6e
Dustin Leavins <dustinleavins@gmail.com>
2013-08-27 10:42:32 -0500

    Added Rakefile test

---
+++ spec/rakefile_spec.rb
@@ -0,0 +1,23 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'rspec'
+require 'mail'
+require 'factory_girl'
+require './models.rb'
+require 'spec_helper'
+
+describe 'email_job' do
+  it 'successfully sends e-mail' do
+    expect(Models::EmailJob.count).to eq(0)
+
+    20.times do
+      FactoryGirl.create(:email_job)
+    end
+
+    expect(Models::EmailJob.count).to eq(20)
+    expect(`rake RACK_ENV=test`).to eq("test\n")
+    `rake email_job RACK_ENV=test`
+    expect(Models::EmailJob.count).to eq(0)
+  end
+end
+
